ce6bf3a65c246f044d6cd45b398ad83962c86909
Deepak <deepak2603dk@gmail.com>
2024-02-04 15:25:10 +0530

    cores middleware

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ const corsOptions = {
   credentials: true,
   optionsSuccessStatus: 204,
 };
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
